6015531fa60fed4ad6d868b3beb01f57e145930b
Iván Sánchez <ivan.ravecca@gmail.com>
2025-02-27 21:24:33 -0300

    Add support for different type of email

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -7,6 +7,15 @@ const EMAIL_TO = process.env.EMAIL_TO;
 const EMAIL_FROM = process.env.EMAIL_FROM;
 
 const router = Router();
+
+interface EmailMessage {
+  to: string | undefined;
+  from: string | undefined;
+  subject: string;
+  text: string;
+  html: string;
+}
+
 router.post("/send", async (req: Request, res: Response) => {
   const { name, email, message } = req.body;
 
@@ -23,14 +32,6 @@ router.post("/send", async (req: Request, res: Response) => {
          <li><strong>Email:</strong> ${email}</li>
          <li><strong>Mensaje:</strong> ${message}</li></ul>`,
   };
-  interface EmailMessage {
-    to: string | undefined;
-    from: string | undefined;
-    subject: string;
-    text: string;
-    html: string;
-  }
-
   sgMail
     .send(msg as EmailMessage)
     .then((): void => {
@@ -39,6 +40,54 @@ router.post("/send", async (req: Request, res: Response) => {
     .catch((error: Error): void => {
       res.status(500).send("Error sending email: " + error.message);
     });
+});
+
+router.post("/inquire", async (req: Request, res: Response) => {
+  const { name, email, phone, course, ci, year, inquire } = req.body;
+
+  if (!name || !email || !phone || !course || !ci || !inquire) {
+    res.status(400).send({
+      error: "Missing required fields",
+      missingFields: {
+        name: !name ? "name is required" : undefined,
+        email: !email ? "email is required" : undefined,
+        phone: !phone ? "phone is required" : undefined,
+        course: !course ? "course is required" : undefined,
+        ci: !ci ? "ci is required" : undefined,
+        inquire: !inquire ? "inquire is required" : undefined,
+      },
+    });
+  } else {
+    const graduation = year ? `Año de graduación: ${year}` : "";
+    const msg = {
+      to: EMAIL_TO,
+      from: EMAIL_FROM,
+      subject: `Bedelías: ${inquire}`,
+      text: `La siguiente es una solicitud desde la web para el trámite de ${inquire}:
+          Nombre: ${name}
+          Email: ${email}
+          Teléfono: ${phone}
+          Curso: ${course}
+          Documento: ${ci}
+          ${graduation}
+          `,
+      html: `La siguiente es una solicitud desde la web para el trámite de <strong>${inquire}</strong>:<br />
+         <ul><li><strong>Nombre:</strong> ${name}</li>
+         <li><strong>Email:</strong> ${email}</li>
+         <li><strong>Teléfono:</strong> ${phone}</li>
+         <li><strong>Curso:</strong> ${course}</li>
+         <li><strong>Documento:</strong> ${ci}</li>
+         <li><strong>${graduation}</strong></li></ul>`,
+    };
+    sgMail
+      .send(msg as EmailMessage)
+      .then((): void => {
+        res.status(202).send();
+      })
+      .catch((error: Error): void => {
+        res.status(500).send("Error sending email: " + error.message);
+      });
+  }
 });
 
 router.post("/test", async (req: Request, res: Response) => {
